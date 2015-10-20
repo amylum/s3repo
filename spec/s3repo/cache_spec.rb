a6be4000ba2ca2c0f6b2c464ba113bcec7e553f7
Les Aker <me@lesaker.org>
2015-10-20 00:10:08 -0400

    complete testing for cache.rb

--- spec/s3repo/cache_spec.rb
+++ spec/s3repo/cache_spec.rb
@@ -9,13 +9,23 @@ describe S3Repo::Cache do
   end
 
   context 'by default' do
-    it 'does not caches the file', vcr: { cassette_name: 'cache' } do
+    it 'tries to cache the file', vcr: { cassette_name: 'cache' } do
       with_auth do
         cache = S3Repo::Cache.new
         cache.serve('repo.db')
         expect { cache.serve('repo.db') }.to raise_exception(
           VCR::Errors::UnhandledHTTPRequestError
         )
+      end
+    end
+    it 'checks etag for caching', vcr: { cassette_name: 'etag_cache' } do
+      with_auth do
+        cache = S3Repo::Cache.new
+        res = (1..2).map do
+          cache.serve('repo.db')
+          cache.send(:etags)['repo.db']
+        end
+        expect(res.first).to eql res.last
       end
     end
   end
@@ -26,6 +36,13 @@ describe S3Repo::Cache do
         cache.serve('repo.db')
         expect(cache.serve('repo.db', false)).to be_an_instance_of String
       end
+    end
+  end
+
+  it 'returns nil if key does not exist', vcr: { cassette_name: 'nil_cache' } do
+    with_auth do
+      cache = S3Repo::Cache.new
+      expect(cache.serve('nonexistent-file')).to be_nil
     end
   end
 end
